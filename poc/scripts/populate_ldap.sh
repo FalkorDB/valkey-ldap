7d55e05e7f8c579f7f586bc8e49967f298dc272d
Muhammad Qadora <memomq70@gmail.com>
2025-12-23 16:51:45 +0200

    Remove obsolete LDAP configuration files and update scripts for user and group management

--- poc/scripts/populate_ldap.sh
+++ poc/scripts/populate_ldap.sh
@@ -8,5 +8,7 @@ done
 ADMIN_PASSWD=admin123!
 ADMIN_DN="cn=admin,dc=valkey,dc=io"
 
-ldapadd -x -w ${ADMIN_PASSWD} -D ${ADMIN_DN} < test/ldap_users.txt
-ldapadd -H ldap://localhost:390 -x -w ${ADMIN_PASSWD} -D ${ADMIN_DN} < test/ldap_users.txt
+ldapadd -x -w ${ADMIN_PASSWD} -D ${ADMIN_DN} < poc/scripts/ldap_users.txt
+ldapadd -H ldap://localhost:390 -x -w ${ADMIN_PASSWD} -D ${ADMIN_DN} < poc/scripts/ldap_users.txt
+ldapadd -x -w ${ADMIN_PASSWD} -D ${ADMIN_DN} < poc/scripts/ldap_groups.txt
+ldapadd -H ldap://localhost:390 -x -w ${ADMIN_PASSWD} -D ${ADMIN_DN} < poc/scripts/ldap_groups.txt
--- scripts/populate_ldap.sh
+++ scripts/populate_ldap.sh
@@ -7,6 +7,11 @@ done
 
 ADMIN_PASSWD=admin123!
 ADMIN_DN="cn=admin,dc=valkey,dc=io"
+CONFIG_PASSWD=config
+
+# Add custom schema for valkeyACL attribute (requires root/config access)
+ldapadd -Y EXTERNAL -H ldapi:/// -f scripts/valkey-schema.ldif 2>/dev/null || \
+  ldapadd -x -w ${CONFIG_PASSWD} -D cn=admin,cn=config -f scripts/valkey-schema.ldif
 
 ldapadd -x -w ${ADMIN_PASSWD} -D ${ADMIN_DN} < test/ldap_users.txt
 ldapadd -H ldap://localhost:390 -x -w ${ADMIN_PASSWD} -D ${ADMIN_DN} < test/ldap_users.txt
