fceb6fe570b5d90d0c97f3206892f183eed3f39f
Muhammad Qadora <memomq70@gmail.com>
2025-12-24 17:29:01 +0200

    Delete non-exempted users from ACL upon LDAP authentication failure

--- src/auth.rs
+++ src/auth.rs
@@ -56,11 +56,13 @@ fn auth_reply_callback(
                 let uname = username.to_string();
                 debug!("failed to authenticate LDAP user {uname}");
                 error!("LDAP authentication failure: {err}");
-                
+
                 // If user is not exempted from LDAP, delete them from ACL
                 // This ensures users deleted from LDAP are also removed from Valkey ACL
                 if !configs::is_user_exempted_from_ldap(&uname) {
-                    debug!("deleting non-exempted user {uname} from ACL after LDAP authentication failure");
+                    debug!(
+                        "deleting non-exempted user {uname} from ACL after LDAP authentication failure"
+                    );
                     // Attempt to delete the user from ACL
                     match ctx.call("ACL", &["DELUSER", &uname]) {
                         Ok(_) => {
@@ -72,7 +74,7 @@ fn auth_reply_callback(
                         }
                     }
                 }
-                
+
                 Ok(AUTH_NOT_HANDLED)
             }
         }
